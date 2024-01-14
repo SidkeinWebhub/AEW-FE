255fd58b4ce87a6044a3b28e79ac790a8a4b0e7c
Divatta <divattadil@gmail.com>
2024-01-14 22:09:12 +0530

    safari bg

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
             <NavLink to={`/`} className="navbar-brand">
                 {/* <span className="logo"><img src={logo} alt="logo" className=' logo1'  />
                 </span> */}
-                <object data={logo} className=' logo1'  type="image/svg+xml" aria-label='logo'></object>
+                <object data={logo} className=' logo1' id='logo1' type="image/svg+xml" aria-label='logo'></object>
             </NavLink>
 
             <div className=" navbar-collapse justify-content-end " >
